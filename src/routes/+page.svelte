24227844b0726f3f92f9a878073985f1b3096ac8
halasgraden <graden@usc.edu>
2026-02-08 14:44:49 -0800

    First page skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section id="container">
+    <div class="text">
+        <h1>WELCOME TO UX SINS</h1>
+        <p>A collection of scenarios that make you <em>feel</em> bad UX.</p>
+    </div>
+
+    <div class="clickable">
+        <button>Click to Get Started</button>
+        <p class="gotcha">Just kidding. Click this instead.</p>
+    </div>
+
+    <div class="bottom-gradient">
+        <span class="circle"></span>
+    </div>
+</section>
